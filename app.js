156ade4479ce3fbe880868d22a64e3afa98d582a
shiba_dog <mischievous.dog00@gmail.com>
2018-05-31 16:53:49 +0900

    できた

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-let express = require('express');
+var restify = require("restify");
+var request = require("request");
 
-let app = express();
-app.use(express.static('dist'));
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3000, function () {
+  console.log('%s listening to %s', server.name, server.url); 
+});
 
-let port = 3000;
-app.listen(port, ()=> {
-  console.log('Expressサーバー起動');
+server.use(restify.plugins.bodyParser({ mapParams: true }));
+server.get('/*.html', restify.plugins.serveStatic({
+  directory: './dist',
+  default: 'index.html',
+}));
+
+server.post('/api/ranking', (req, res, next) => {
+  let headers = {
+      'Content-Type': 'application/json'
+  };
+  let teamOptions = {
+    url: 'https://msopenhack.azurewebsites.net/api/trivia/leaderboard/user',
+    method: 'POST',
+    headers: headers,
+    json: true,
+    form: req.body,
+  };
+  request(teamOptions, function (error, response, body) {
+    res.contentType = "json";
+    res.body = JSON.stringify(body);
+    res.send(body);
+  });
+  return next();
 });
